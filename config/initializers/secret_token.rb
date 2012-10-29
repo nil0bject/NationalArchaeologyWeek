6b8f24f48c27b7b3d0d9011ba950d7b059b56a53
tycho <nick@gea-interactive.com.au>
2012-10-29 21:06:23 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NationalArchaeologyWeek::Application.config.secret_token = '59a2d02177b8face5171f89c4947a2470b7648aca15ee7a26027a4da91257cdeedb7b3c6d16b758b914143d5bbb58bab9abc0d946dcf9caaa021304a39badf40'
